a0a35be9f059eec5c35f51d5d7c5277c184af3d6
alex <alex@pdp7.net>
2024-04-12 19:10:14 +0200

    Add OpenTelemetry feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,43 @@
+use tracing_subscriber::prelude::*;
+
 pub fn run<T>(f: impl FnOnce() -> T) -> T {
+    let mut layers = Vec::new();
+
     #[cfg(not(feature = "indicatif"))]
-    return {
-        tracing_subscriber::fmt::init();
-        f()
-    };
+    layers.push(tracing_subscriber::fmt::layer().boxed());
 
     #[cfg(feature = "indicatif")]
-    return {
+    {
         use tracing_indicatif::IndicatifLayer;
         use tracing_subscriber::prelude::*;
 
         let indicatif_layer = IndicatifLayer::new();
 
-        tracing_subscriber::registry()
-            .with(tracing_subscriber::fmt::layer().with_writer(indicatif_layer.get_stderr_writer()))
-            .with(indicatif_layer)
-            .init();
-
-        f()
+        let layer = tracing_subscriber::fmt::layer()
+            .with_writer(indicatif_layer.get_stderr_writer())
+            .boxed();
+        layers.push(layer);
+        layers.push(indicatif_layer.boxed());
     };
+
+    #[cfg(feature = "opentelemetry")]
+    {
+        let tracer = opentelemetry_otlp::new_pipeline()
+            .tracing()
+            .with_exporter(opentelemetry_otlp::new_exporter().http())
+            .install_simple()
+            .unwrap();
+
+        let telemetry_layer = tracing_opentelemetry::layer().with_tracer(tracer).boxed();
+        layers.push(telemetry_layer);
+    }
+
+    #[allow(clippy::let_and_return)] // https://github.com/rust-lang/rust-clippy/pull/12558/
+    tracing::subscriber::with_default(tracing_subscriber::registry().with(layers), || {
+        let result = f();
+
+        #[cfg(feature = "opentelemetry")]
+        opentelemetry::global::shutdown_tracer_provider();
+        result
+    })
 }
